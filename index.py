9108cb6094766376be25e7c479856aa86fa43989
¨JoAbdala¨ <marsofio@hotmail.com>
2023-05-17 18:18:53 -0300

    practicando variables

---
+++ index.py
@@ -0,0 +1,27 @@
+
+nombre = "Jose"
+apellido = "Abdala"
+edad= 55
+altura = 1.67
+peso = 72 
+
+print ("Nombre y Apellido: ", nombre,  apellido)
+print ("Edad", edad, "años" )
+print ("Estatura", altura, "Peso",  peso, "Kg")
+
+num = 3 
+num2 = 8 
+num3 = 10 
+
+num = (num+ 10) * 5
+num2 = (num2 -5) + num  
+num3= (num3+ num2) /4 
+
+print( "Resultado 1:=>" , num , "Resultado 2:=>" ,  num2,  "Resultado 3:=>" , num3 )
+
+
+
+
+
+
+
